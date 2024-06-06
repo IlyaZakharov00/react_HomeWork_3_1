3b2120c40f88a67bf972e9a6515883f524652367
Ilya frontend <zaxarov.ilya.00@inbox.ru>
2024-06-06 21:01:06 +0500

    task 1 done 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import { Stars } from './components/stars/stars'
 
 
 function App() {
-  const [count, setCount] = useState(1)
+  // const [count, setCount] = useState(1)
+  let count = 0
 
   return (
     <>
